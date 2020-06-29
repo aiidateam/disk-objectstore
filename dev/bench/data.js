a5df2281f9bb99a0cff7d11c60c8834e6ac0c2c5
github-action-benchmark <github@users.noreply.github.com>
2020-06-29 05:46:06 +0000

    add Benchmark on windows-latest with Python 3.5 (pytest) benchmark result for eaece2837daa7abe2adaedffe3309141307af01b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1593409557476,
+  "lastUpdate": 1593409565902,
   "repoUrl": "https://github.com/giovannipizzi/disk-objectstore",
   "entries": {
     "Benchmark on ubuntu-latest with Python 3.8": [
@@ -358,6 +358,46 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.02561361416311727",
             "extra": "mean: 291.8996199999988 msec\nrounds: 5"
+          }
+        ]
+      }
+    ],
+    "Benchmark on windows-latest with Python 3.5": [
+      {
+        "commit": {
+          "author": {
+            "email": "giovanni.pizzi@epfl.ch",
+            "name": "Giovanni Pizzi",
+            "username": "giovannipizzi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "eaece2837daa7abe2adaedffe3309141307af01b",
+          "message": "Merge pull request #39 from giovannipizzi/fix_if_github_actions\n\nFixed if conditional for runs",
+          "timestamp": "2020-06-29T07:38:51+02:00",
+          "tree_id": "5a416ec1162b8b9f1b0cb48c6b1ff6529f26aba7",
+          "url": "https://github.com/giovannipizzi/disk-objectstore/commit/eaece2837daa7abe2adaedffe3309141307af01b"
+        },
+        "date": 1593409564503,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmark.py::test_pack_write",
+            "value": 0.23224873381086977,
+            "unit": "iter/sec",
+            "range": "stddev: 0.04850852996910224",
+            "extra": "mean: 4.305728533333333 sec\nrounds: 3"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_pack_read",
+            "value": 2.302478539863892,
+            "unit": "iter/sec",
+            "range": "stddev: 0.022590357523775497",
+            "extra": "mean: 434.3145800000002 msec\nrounds: 5"
           }
         ]
       }
