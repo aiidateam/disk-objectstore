31f3c99df911a6caa977a17878d5309a8e74a7e4
github-action-benchmark <github@users.noreply.github.com>
2020-06-29 05:42:42 +0000

    add Benchmark on macos-latest with Python 3.7 (pytest) benchmark result for eaece2837daa7abe2adaedffe3309141307af01b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1593409357995,
+  "lastUpdate": 1593409362844,
   "repoUrl": "https://github.com/giovannipizzi/disk-objectstore",
   "entries": {
     "Benchmark on ubuntu-latest with Python 3.8": [
@@ -38,6 +38,46 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.013639855111174074",
             "extra": "mean: 301.13516879999906 msec\nrounds: 5"
+          }
+        ]
+      }
+    ],
+    "Benchmark on macos-latest with Python 3.7": [
+      {
+        "commit": {
+          "author": {
+            "email": "giovanni.pizzi@epfl.ch",
+            "name": "Giovanni Pizzi",
+            "username": "giovannipizzi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "eaece2837daa7abe2adaedffe3309141307af01b",
+          "message": "Merge pull request #39 from giovannipizzi/fix_if_github_actions\n\nFixed if conditional for runs",
+          "timestamp": "2020-06-29T07:38:51+02:00",
+          "tree_id": "5a416ec1162b8b9f1b0cb48c6b1ff6529f26aba7",
+          "url": "https://github.com/giovannipizzi/disk-objectstore/commit/eaece2837daa7abe2adaedffe3309141307af01b"
+        },
+        "date": 1593409362202,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmark.py::test_pack_write",
+            "value": 0.22946784575110954,
+            "unit": "iter/sec",
+            "range": "stddev: 0.052770897498388834",
+            "extra": "mean: 4.357909042666666 sec\nrounds: 3"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_pack_read",
+            "value": 1.805596308315637,
+            "unit": "iter/sec",
+            "range": "stddev: 0.019119665201319243",
+            "extra": "mean: 553.8336533999988 msec\nrounds: 5"
           }
         ]
       }
