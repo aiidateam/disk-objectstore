962bda804707a8f182b50c6db8b51a0cfa134df7
github-action-benchmark <github@users.noreply.github.com>
2020-06-29 07:06:41 +0000

    add Benchmark on macos-latest (pytest) benchmark result for 37e4914e091106642af3975663590f013d0813b3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1593414372290,
+  "lastUpdate": 1593414400997,
   "repoUrl": "https://github.com/giovannipizzi/disk-objectstore",
   "entries": {
     "Benchmark on ubuntu-latest": [
@@ -38,6 +38,46 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.01126805757191704",
             "extra": "mean: 324.4712996000004 msec\nrounds: 5"
+          }
+        ]
+      }
+    ],
+    "Benchmark on macos-latest": [
+      {
+        "commit": {
+          "author": {
+            "email": "giovanni.pizzi@epfl.ch",
+            "name": "Giovanni Pizzi",
+            "username": "giovannipizzi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "37e4914e091106642af3975663590f013d0813b3",
+          "message": "Merge pull request #40 from giovannipizzi/move_benchmarks_new_workflow\n\nMoving benchmarks to a different workflow",
+          "timestamp": "2020-06-29T09:05:10+02:00",
+          "tree_id": "21cfdf2eed33a3725bbd4cbbf25afc1b0bedfa02",
+          "url": "https://github.com/giovannipizzi/disk-objectstore/commit/37e4914e091106642af3975663590f013d0813b3"
+        },
+        "date": 1593414400104,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmark.py::test_pack_write",
+            "value": 0.3306547594895717,
+            "unit": "iter/sec",
+            "range": "stddev: 0.01527932818493315",
+            "extra": "mean: 3.0243024523333326 sec\nrounds: 3"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_pack_read",
+            "value": 2.7073370080458896,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00864343419938554",
+            "extra": "mean: 369.3666496000006 msec\nrounds: 5"
           }
         ]
       }
