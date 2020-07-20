9dd029d81a791f0fa3324209fa7edfd015796aa8
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2020-07-20 18:33:16 +0200

    Removing the 'OS-independent' flag
    This is not really true, it actually is OS dependent,
    and I tested the three most common ones

--- setup.py
+++ setup.py
@@ -52,8 +52,7 @@ setup(
         'Programming Language :: Python :: 3', 'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6', 'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8', 'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent', 'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows', 'Operating System :: POSIX :: Linux',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Operating System :: MacOS :: MacOS X', 'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux', 'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 )
