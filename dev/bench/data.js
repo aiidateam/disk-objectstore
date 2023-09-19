e902a261b4d3e8495d32f94ba3a58295c3f37589
github-action-benchmark <github@users.noreply.github.com>
2023-09-19 12:19:37 +0000

    add Benchmark on macos-latest (pytest) benchmark result for 5dc4a34d212cf75e9e649b4294fd8bfa452d97ad

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695125959860,
+  "lastUpdate": 1695125977530,
   "repoUrl": "https://github.com/aiidateam/disk-objectstore",
   "entries": {
     "Benchmark on ubuntu-latest": [
@@ -9076,6 +9076,79 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 5.5106960306565416e-8",
             "extra": "mean: 227.67491204030586 nsec\nrounds: 192050"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "66853113+pre-commit-ci[bot]@users.noreply.github.com",
+            "name": "pre-commit-ci[bot]",
+            "username": "pre-commit-ci[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5dc4a34d212cf75e9e649b4294fd8bfa452d97ad",
+          "message": "[pre-commit.ci] pre-commit autoupdate (#157)\n\nupdates:\r\n- https://github.com/ikamensh/flynt/: 0.78 → 1.0.1\r\n- [github.com/executablebooks/mdformat: 0.7.16 → 0.7.17](https://github.com/executablebooks/mdformat/compare/0.7.16...0.7.17)\r\n- [github.com/asottile/pyupgrade: v3.4.0 → v3.10.1](https://github.com/asottile/pyupgrade/compare/v3.4.0...v3.10.1)\r\n- [github.com/psf/black: 23.3.0 → 23.7.0](https://github.com/psf/black/compare/23.3.0...23.7.0)\r\n- [github.com/PyCQA/pylint: v3.0.0a6 → v3.0.0a7](https://github.com/PyCQA/pylint/compare/v3.0.0a6...v3.0.0a7)\r\n- [github.com/pre-commit/mirrors-mypy: v1.3.0 → v1.5.1](https://github.com/pre-commit/mirrors-mypy/compare/v1.3.0...v1.5.1)\r\n\r\nCo-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>",
+          "timestamp": "2023-09-19T14:17:28+02:00",
+          "tree_id": "79dbae413ecad9e644deefaef21eeaeb54ab4120",
+          "url": "https://github.com/aiidateam/disk-objectstore/commit/5dc4a34d212cf75e9e649b4294fd8bfa452d97ad"
+        },
+        "date": 1695125976032,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_benchmark.py::test_pack_write",
+            "value": 1.0583791697889358,
+            "unit": "iter/sec",
+            "range": "stddev: 0.2048648252231785",
+            "extra": "mean: 944.8409686666661 msec\nrounds: 3"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_loose_write",
+            "value": 1.0675147425093643,
+            "unit": "iter/sec",
+            "range": "stddev: 0.059452718484458024",
+            "extra": "mean: 936.7552130000003 msec\nrounds: 3"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_pack_read",
+            "value": 4.359713116460808,
+            "unit": "iter/sec",
+            "range": "stddev: 0.06026380538317154",
+            "extra": "mean: 229.37289066666722 msec\nrounds: 6"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_loose_read",
+            "value": 9.769895672006266,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00923027619510385",
+            "extra": "mean: 102.35523833333302 msec\nrounds: 9"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_has_objects",
+            "value": 1.7699574000989062,
+            "unit": "iter/sec",
+            "range": "stddev: 0.07754774103849901",
+            "extra": "mean: 564.9853493333339 msec\nrounds: 3"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_list_all_packed",
+            "value": 2524136.740388056,
+            "unit": "iter/sec",
+            "range": "stddev: 3.99207902901938e-7",
+            "extra": "mean: 396.17505026540755 nsec\nrounds: 154727"
+          },
+          {
+            "name": "tests/test_benchmark.py::test_list_all_loose",
+            "value": 2982647.429785206,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00000574778225472009",
+            "extra": "mean: 335.2726138576804 nsec\nrounds: 195199"
           }
         ]
       }
