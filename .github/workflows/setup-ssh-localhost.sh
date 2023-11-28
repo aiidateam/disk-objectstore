c030168847c506c1c4c8f8a14218c82416467ec0
Kristjan Eimre <kristjaneimre@gmail.com>
2023-11-28 21:12:13 +0100

    fix CI macos ssh localhost

--- .github/workflows/setup-ssh-localhost.sh
+++ .github/workflows/setup-ssh-localhost.sh
@@ -4,3 +4,6 @@ set -ev
 ssh-keygen -q -t rsa -b 4096 -N "" -f "${HOME}/.ssh/id_rsa"
 ssh-keygen -y -f "${HOME}/.ssh/id_rsa" >> "${HOME}/.ssh/authorized_keys"
 ssh-keyscan -H localhost >> "${HOME}/.ssh/known_hosts"
+
+chmod 700 "${HOME}/.ssh"
+chmod 600 "${HOME}/.ssh"/*
